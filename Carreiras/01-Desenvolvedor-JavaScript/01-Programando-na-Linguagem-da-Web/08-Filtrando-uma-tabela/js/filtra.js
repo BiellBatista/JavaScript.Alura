d01193b341533985ebc3dcb539b5e6d2aa18ae1b
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-01 20:37:25 -0300

    conclusão do módulo 03 da aula 08 da seção "JavaScript: Programando na linguagem da web"

--- Carreiras/01-Desenvolvedor-JavaScript/01-Programando-na-Linguagem-da-Web/08-Filtrando-uma-tabela/js/filtra.js
+++ Carreiras/01-Desenvolvedor-JavaScript/01-Programando-na-Linguagem-da-Web/08-Filtrando-uma-tabela/js/filtra.js
@@ -1,13 +1,15 @@
 let campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", () => {
-    console.log(campoFiltro.value);
     let pacientes = document.querySelectorAll(".paciente");
 
     pacientes.forEach(paciente => {
         let tdNome = paciente.querySelector(".info-nome");
         let nome = tdNome.textContent;
-
-        console.log(nome);
+        if(nome != campoFiltro.value && campoFiltro.value.length > 0) {
+            paciente.classList.add("invisivel");
+        } else {
+            paciente.classList.remove("invisivel");
+        }
     }); 
 });
