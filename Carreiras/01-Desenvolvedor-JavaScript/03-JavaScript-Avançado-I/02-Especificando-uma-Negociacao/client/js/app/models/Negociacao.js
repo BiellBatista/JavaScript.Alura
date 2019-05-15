5c75a9720d0216f00578315dac4fe06c6e16f828
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-15 00:11:11 -0300

    conclusão do módulo 01 da aula 02 da seção "JavaScript avançado I: ES6, orientação a objetos e padrões de projetos"

---
+++ Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/02-Especificando-uma-Negociacao/client/js/app/models/Negociacao.js
@@ -0,0 +1,8 @@
+class Negociacao {
+    constructor (){
+        //declarando os atributos da classe
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0;
+    }
+}
