7ad578fffb9025d0cda4782a2d889933803d43ce
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-15 01:42:24 -0300

    conclusão do módulo 02 da aula 03 da seção "JavaScript avançado I: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/03 - A-Ligacao-Entre-Acoes-Usuario-Modelo/client/js/app/controllers/NegociacaoController.js
+++ Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/03 - A-Ligacao-Entre-Acoes-Usuario-Modelo/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,14 @@
 class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei ação no controller!');
+
+        let $ = document.querySelector.bind(document); // associando a função a variável $. Estilo jQuery. Devo colocar o bind para que a função saiba que quero manter o contexto
+        let inputData = $('#data');
+        let inputQuantidade = document.querySelector('#quantidade');
+        let inputValor = document.querySelector('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
