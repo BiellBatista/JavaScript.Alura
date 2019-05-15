a0bde4ad7886060e09f7ac45586583c5731f661b
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-15 01:46:40 -0300

    conclusão do módulo 03 da aula 03 da seção "JavaScript avançado I: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/03 - A-Ligacao-Entre-Acoes-Usuario-Modelo/client/js/app/controllers/NegociacaoController.js
+++ Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/03 - A-Ligacao-Entre-Acoes-Usuario-Modelo/client/js/app/controllers/NegociacaoController.js
@@ -1,14 +1,22 @@
 class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document); // associando a função a variável $. Estilo jQuery. Devo colocar o bind para que a função saiba que quero manter o contexto
+
+        this.inputData = $('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault();
 
-        let $ = document.querySelector.bind(document); // associando a função a variável $. Estilo jQuery. Devo colocar o bind para que a função saiba que quero manter o contexto
-        let inputData = $('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor = document.querySelector('#valor');
-
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
     }
 }
+
+/*
+Devo evitar de pecorrer o DOM. Por isso, eu uso no constructor()
+*/
