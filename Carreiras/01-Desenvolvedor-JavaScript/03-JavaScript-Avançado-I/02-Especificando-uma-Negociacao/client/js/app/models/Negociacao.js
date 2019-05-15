360efd2886bf342e62d0d3df17919d76810dbf5b
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-15 00:59:08 -0300

    conclusão do módulo 08 da aula 02 da seção "JavaScript avançado I: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/02-Especificando-uma-Negociacao/client/js/app/models/Negociacao.js
+++ Carreiras/01-Desenvolvedor-JavaScript/03-JavaScript-Avançado-I/02-Especificando-uma-Negociacao/client/js/app/models/Negociacao.js
@@ -1,7 +1,7 @@
 class Negociacao {
     constructor (data, quantidade, valor){
         //declarando os atributos da classe
-        this._data = data;
+        this._data = new Date(data.getTime()); // Gerando um novo objeto, a partir do recebido. Isso é para remover a mesma referência e, consequetemente, evitar que o atributo seja alterado por fora, pois a referência é a mesma
         this._quantidade = quantidade;
         this._valor = valor;
 
@@ -23,11 +23,16 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        // return this._data;
+        return new Date(this._data.getTime()); // retornando um objeto para evitar alterações. Isso se chama, programação defensiva
     }
 }
 
 /*
 O método Object.freeze() é ralo, fica na superficie, porque eu posso alterar as propriedades de uma propriedade
-do tipo obejto. Como por exemplo, a data, pois o freeze() congela o objeto do this e não os objetos da componitização
+do tipo obejto. Como por exemplo, a data, pois o freeze() congela o objeto do this e não os objetos da componetização
+
+
+Em programação defensiva, devo retornar uma copia do atributo (como fiz na data) e devo gerar uma nova referência, no construtor, de um atributo
+para evitar que a referência seja a mesma que a de fora (como fiz na data)
 */
