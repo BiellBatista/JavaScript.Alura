c04a05b978519d0e3376c896b2a6767ea0595825
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-19 08:33:04 -0300

    conclusão do módulo 02 da aula 04 "JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/04-JavaScript-Avançado-II/04-Importando-Negociacoes/client/js/app/controllers/NegociacaoController.js
+++ Carreiras/01-Desenvolvedor-JavaScript/04-JavaScript-Avançado-II/04-Importando-Negociacoes/client/js/app/controllers/NegociacaoController.js
@@ -29,6 +29,37 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest(); // ajax
+
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => { //esta arrow function será executada a cada status do AJAX
+            /*
+             Status do AJAX
+
+             0: requisição ainda não iniciada
+
+             1: conexão com o servidor estabelecida
+
+             2: requisição recebida
+
+             3: processando requisição
+
+             4: requisição concluída e a resposta está pronta
+            */
+            //verificando se o status da requisição é 4
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor!');
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor!');
+                }
+            }
+
+        };
+        xhr.send();
+    }
+
     apaga() {
         this._listaNegociacoes.esvazia();
 
