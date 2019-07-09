25114607242a3821997913199b6fe623a354725e
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-08 22:55:44 -0300

    conclusão do módulo 05 da aula 02 da seção "JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/02-Gerenciando-Conexao-Factory/client/js/app/services/ConnectionFactory.js
+++ Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/02-Gerenciando-Conexao-Factory/client/js/app/services/ConnectionFactory.js
@@ -1,26 +1,40 @@
 var ConnectionFactory = (function () {
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes']; //o const bloqueia a reatribuição, mas não a imutabilidade
+    const version = 4;
+    const dbName = 'aluraframe';
+
     var connection = null;
+    var close = null;
 
     //devo dar o return da Classe ConnectionFactory para poder acessá-la
     return class ConnectionFactory {
         constructor() {
             throw new Error('Não é possível criar instâncias de ConnectionFactory.');
         }
-    
+
         static getConnection() {
             return new Promise((resolve, reject) => {
                 let openRequest = window.indexedDB.open(dbName, version);
-    
+
                 openRequest.onupgradeneeded = e => {
                     ConnectionFactory._createStores(e.target.result);
                 };
                 
                 openRequest.onsuccess = e => {
                     if(!connection)
+                    {
                         connection = e.target.result;
+                        //associando o closse com a connection (o this é a connection)
+                        close = connection.close.bind(connection);
+                        /**
+                         * o close de baixo é feito para testar a API reflect
+                         */
+                        // close = connection.close;
+                        connection.close = function () {
+                            throw new Error('Você não pode fechar diretamente a conexão.');
+                        }; //Monkey Patch é a ação de alterar uma função de uma biblioteca. Neste caso, estou substituindo o método do close() da connection
+                    }
+
                     resolve(connection);
                 };
                 
@@ -30,7 +44,16 @@ var ConnectionFactory = (function () {
                 };
             })
         }
-    
+
+        static closeConnection() {
+            if(connection) {
+                close();
+                //aqui, logo abaixo, é usado sem o bind.
+                // Reflect.apply(close, connection, []);
+                connection = null;
+            }
+        }
+
         static _createStores(connection) {
             stores.forEach(store => {
                 if (connection.objectStoreNames.contains(store))
