e533a267588fa25904cdcbbe699460808303d8e0
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-08 23:39:28 -0300

    conclusão do módulo 01 da aula 03 da seção "JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/03-Padronizando-Acesso-Dados-Pattern-DAO/client/js/app/services/ConnectionFactory.js
+++ Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/03-Padronizando-Acesso-Dados-Pattern-DAO/client/js/app/services/ConnectionFactory.js
@@ -6,7 +6,6 @@ var ConnectionFactory = (function () {
     var connection = null;
     var close = null;
 
-    //devo dar o return da Classe ConnectionFactory para poder acessá-la
     return class ConnectionFactory {
         constructor() {
             throw new Error('Não é possível criar instâncias de ConnectionFactory.');
@@ -19,25 +18,20 @@ var ConnectionFactory = (function () {
                 openRequest.onupgradeneeded = e => {
                     ConnectionFactory._createStores(e.target.result);
                 };
-                
+
                 openRequest.onsuccess = e => {
                     if(!connection)
                     {
                         connection = e.target.result;
-                        //associando o closse com a connection (o this é a connection)
                         close = connection.close.bind(connection);
-                        /**
-                         * o close de baixo é feito para testar a API reflect
-                         */
-                        // close = connection.close;
                         connection.close = function () {
                             throw new Error('Você não pode fechar diretamente a conexão.');
-                        }; //Monkey Patch é a ação de alterar uma função de uma biblioteca. Neste caso, estou substituindo o método do close() da connection
+                        };
                     }
 
                     resolve(connection);
                 };
-                
+
                 openRequest.onerror = e => {
                     console.log(e.target.error);
                     reject(e.target.error.name)
@@ -48,8 +42,6 @@ var ConnectionFactory = (function () {
         static closeConnection() {
             if(connection) {
                 close();
-                //aqui, logo abaixo, é usado sem o bind.
-                // Reflect.apply(close, connection, []);
                 connection = null;
             }
         }
@@ -63,29 +55,3 @@ var ConnectionFactory = (function () {
         }
     }
 })();
-
-/**
- * Quando eu crio uma função anonima (exemplo de cima) e coloco ela dentro de um paretenses
- * e apos o encapsulamento do paretenteses, fecho e abro outro par de paresentes (function(){})();
- * estou criando uma funcao alto invocada. Ela será carregada e ao mesmo tempo executada.
- * Estou armazenando o resultado da funcao alto invocada em uma variável global
- */
-
- /**
-  * let pessoa2 = new Pessoa('Almeida', 'Flávio');
-
-pessoa2.obterNomeCompleto = function() {
-
-  return `${this.nome} - ${this.sobrenome}`;
-}
-console.log(pessoa2.obterNomeCompleto());
-Estamos modificando dinamicamente obterNomeCompleto apenas na instância pessoa2. Veja que usamos function e não arrow function. Foi necessário usar function devido ao seu escopo dinâmico, isto é, this deve variar de acordo com a instância no qual obterNomeCompleto é chamado. Se usarmos arrow function, seu escopo léxico fará com que o this seja sempre do contexto no qual a função é declarada, no caso window, o escopo global.
-
-Inclusive é possível alterar a definição do método diretamente na própria classe, dessa maneira, todas as instâncias de Pessoa, criadas antes ou depois da modificação, automaticamente "herdarão" a modificação:
-
-Pessoa.prototype.obterNomeCompleto = function() {
-
-  return `${this.nome} - ${this.sobrenome}`;
-}
-O monkey patch deve ser usado com parcimônia, até mesmo como último recurso, quando propõe uma modificação direta na definição da classe. Isto porque modificações usadas globalmente podem ocasionar bugs em diversas partes do nosso código, tendo assim, um resultado imprevisível.
-  */
