e5afd68dcbb284b6248f7d2e76a0c50c6f7f274b
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-09 01:45:30 -0300

    conclusão do módulo 04 da aula 03 da seção "JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/03-Padronizando-Acesso-Dados-Pattern-DAO/client/js/app/controllers/NegociacaoController.js
+++ Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/03-Padronizando-Acesso-Dados-Pattern-DAO/client/js/app/controllers/NegociacaoController.js
@@ -70,8 +70,15 @@ class NegociacaoController {
     }
 
     apaga() {
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            })
+            .catch(error => this._mensagem = error);
     }
 
     _criaNegociacao() {
