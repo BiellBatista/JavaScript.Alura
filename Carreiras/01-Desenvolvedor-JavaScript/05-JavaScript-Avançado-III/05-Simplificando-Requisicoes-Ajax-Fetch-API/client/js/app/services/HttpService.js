55d6a2aba8955a701e7c364fcec62bb939c2bb1f
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-10 02:03:19 -0300

    conclusão do módulo 01 da aula 05 da seção "JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/05-Simplificando-Requisicoes-Ajax-Fetch-API/client/js/app/services/HttpService.js
+++ Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/05-Simplificando-Requisicoes-Ajax-Fetch-API/client/js/app/services/HttpService.js
@@ -1,21 +1,19 @@
 class HttpService {
+    _handleErros(res) {
+        if(res.ok) {
+            return res;
+        } else {
+            throw new Error(res.statusText); //retornando a mensagem de erro do servidor
+        }
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send();
-        });
+        //fetch é uma API de escobo global. ELa facilita as requisições. Não consigo cancelar a requisição
+        //A resposta retornada pelo fetch é bruta (não é JSON, nem XML..), devo solicitar para ela se formatar
+        return fetch(url)
+        //sempre que eu trabalhar com a fetch API, devo verificar se retornou algum erro
+            .then(res => this._handleErros(res)) //vendo se o servidor retornou algum erro
+            .then(res => res.json()); //o .json() faz com que a resposta se converta automaticamente
     }
 
     post(url, dado) {
