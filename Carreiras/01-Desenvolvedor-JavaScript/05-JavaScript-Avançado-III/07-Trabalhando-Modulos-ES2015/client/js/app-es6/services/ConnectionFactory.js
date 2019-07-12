141c41204d595e431d8aa2f992ef275306cc6094
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-12 08:26:51 -0300

    conclusão do módulo 03 da aula 07 da seção "JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos"

--- Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/07-Trabalhando-Modulos-ES2015/client/js/app-es6/services/ConnectionFactory.js
+++ Carreiras/01-Desenvolvedor-JavaScript/05-JavaScript-Avançado-III/07-Trabalhando-Modulos-ES2015/client/js/app-es6/services/ConnectionFactory.js
@@ -1,57 +1,60 @@
-var ConnectionFactory = (function () {
-    const stores = ['negociacoes']; //o const bloqueia a reatribuição, mas não a imutabilidade
-    const version = 4;
-    const dbName = 'aluraframe';
+/**
+ * Como no ES6 tudo é módulo, essas variáveis fora da classe não são globais
+ * ou seja, são acessiveis apenas neste arquivo (módulo)
+ */
+const stores = ['negociacoes']; //o const bloqueia a reatribuição, mas não a imutabilidade
+const version = 4;
+const dbName = 'aluraframe';
 
-    var connection = null;
-    var close = null;
+let connection = null;
+let close = null;
 
-    return class ConnectionFactory {
-        constructor() {
-            throw new Error('Não é possível criar instâncias de ConnectionFactory.');
-        }
+// Podemos importar a class ConnectionFacotry, mas não iremos usar as variáveis de cima
+export class ConnectionFactory {
+    constructor() {
+        throw new Error('Não é possível criar instâncias de ConnectionFactory.');
+    }
 
-        static getConnection() {
-            return new Promise((resolve, reject) => {
-                let openRequest = window.indexedDB.open(dbName, version);
+    static getConnection() {
+        return new Promise((resolve, reject) => {
+            let openRequest = window.indexedDB.open(dbName, version);
 
-                openRequest.onupgradeneeded = e => {
-                    ConnectionFactory._createStores(e.target.result);
-                };
+            openRequest.onupgradeneeded = e => {
+                ConnectionFactory._createStores(e.target.result);
+            };
 
-                openRequest.onsuccess = e => {
-                    if(!connection)
-                    {
-                        connection = e.target.result;
-                        close = connection.close.bind(connection);
-                        connection.close = function () {
-                            throw new Error('Você não pode fechar diretamente a conexão.');
-                        };
-                    }
+            openRequest.onsuccess = e => {
+                if(!connection)
+                {
+                    connection = e.target.result;
+                    close = connection.close.bind(connection);
+                    connection.close = function () {
+                        throw new Error('Você não pode fechar diretamente a conexão.');
+                    };
+                }
 
-                    resolve(connection);
-                };
+                resolve(connection);
+            };
 
-                openRequest.onerror = e => {
-                    console.log(e.target.error);
-                    reject(e.target.error.name)
-                };
-            })
-        }
+            openRequest.onerror = e => {
+                console.log(e.target.error);
+                reject(e.target.error.name)
+            };
+        })
+    }
 
-        static closeConnection() {
-            if(connection) {
-                close();
-                connection = null;
-            }
-        }
-
-        static _createStores(connection) {
-            stores.forEach(store => {
-                if (connection.objectStoreNames.contains(store))
-                    connection.deleteObjectStore(store);
-                connection.createObjectStore(store, { autoIncrement: true });
-            });
+    static closeConnection() {
+        if(connection) {
+            close();
+            connection = null;
         }
     }
-})();
+
+    static _createStores(connection) {
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+            connection.createObjectStore(store, { autoIncrement: true });
+        });
+    }
+}
