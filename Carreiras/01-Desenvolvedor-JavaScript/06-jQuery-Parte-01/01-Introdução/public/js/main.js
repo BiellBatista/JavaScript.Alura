33d9cb87a7a420b77ddecc0eea1387a843e5f118
Gabriel Batista <gabriel.texteis@gmail.com>
2019-08-11 23:53:39 -0300

    conclusão do módulo 04 da aula 01 da seção "jQuery: Domine a biblioteca mais popular do mercado parte 1"

--- Carreiras/01-Desenvolvedor-JavaScript/06-jQuery-Parte-01/01-Introdução/public/js/main.js
+++ Carreiras/01-Desenvolvedor-JavaScript/06-jQuery-Parte-01/01-Introdução/public/js/main.js
@@ -1,1 +1,7 @@
-console.log('Ola mundo');
+// console.log('Ola mundo'); //módulo 03
+
+let frase = $('.frase').text(); //pegando o conteúdo da tag <p>
+let numerosPlavras = frase.split(" ").length;
+let tamanhoFrase = $('#tamanhoFrase'); // função text serve para pegar o texto entre a tag <p>TEXTO</p> <span>TEXTO</span>
+
+tamanhoFrase.text(numerosPlavras);
