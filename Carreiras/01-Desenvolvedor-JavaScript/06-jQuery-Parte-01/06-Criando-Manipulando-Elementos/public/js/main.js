9067dd49f90db64a139cf891b699d5cf5c105d15
Gabriel Batista <gabriel.batista@kognita.com>
2019-08-26 15:42:42 -0300

    conclusão da aula 06 da seção "jQuery: Domine a biblioteca mais popular do mercado parte 1"

--- Carreiras/01-Desenvolvedor-JavaScript/06-jQuery-Parte-01/06-Criando-Manipulando-Elementos/public/js/main.js
+++ Carreiras/01-Desenvolvedor-JavaScript/06-jQuery-Parte-01/06-Criando-Manipulando-Elementos/public/js/main.js
@@ -63,43 +63,6 @@ function finalizaJogo(){
     inserePlacar();
 }
 
-function inserePlacar() {
-    //o .find() irá procurar a tag que eu especifiquei
-    let corpoTabela = $(".placar").find("tbody");
-    let numerosPalavras = $("#contador-palavras").text();
-    let usuario = 'Gabriel';
-    let linha = novaLinha(usuario, numerosPalavras);
-
-    linha.find('.botao-remover').click(removeLinha);
-    //append() = adicionar o elemento no final dentro de um elemento
-    //prepend() = adicionar como primeiro elemento dentro de um outro elemento
-    corpoTabela.append(linha);
-}
-
-function novaLinha(nomeUsuario, numerosPalavras) {
-    //o bom de criar elementos HTML é que eu posso usar as funcionalidades do objeto
-    let linha = $('<tr>'); //criando um elemento html
-    let colunaUsuario = $('<td>').text(nomeUsuario); //criando um novo elemento. O jQuery sabe que é para criar um elemento por causda do <>
-    let colunaPalavras = $('<td>').text(numerosPalavras);
-    let colunaRemover = $('<td>');
-
-    let link = $('<a>').addClass('botao-remover').attr('href', '#');
-    let icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
-
-    link.append(icone);
-    colunaRemover.append(link);
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha() {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
 let reiniciaJogo = function () {
     campoDigitacao.attr('disabled', false);
     campoDigitacao.val('');
