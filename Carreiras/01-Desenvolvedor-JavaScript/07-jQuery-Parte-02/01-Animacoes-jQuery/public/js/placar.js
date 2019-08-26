72f50445716933745391043b46d48299f2a78092
Gabriel Batista <gabriel.batista@kognita.com>
2019-08-26 15:52:39 -0300

    ambiente preparado para a seção "jQuery: Avance na biblioteca mais popular do mercado parte 2"

---
+++ Carreiras/01-Desenvolvedor-JavaScript/07-jQuery-Parte-02/01-Animacoes-jQuery/public/js/placar.js
@@ -0,0 +1,32 @@
+function inserePlacar() {
+    let corpoTabela = $(".placar").find("tbody");
+    let numerosPalavras = $("#contador-palavras").text();
+    let usuario = 'Gabriel';
+    let linha = novaLinha(usuario, numerosPalavras);
+
+    linha.find('.botao-remover').click(removeLinha);
+    corpoTabela.append(linha);
+}
+
+function novaLinha(nomeUsuario, numerosPalavras) {
+    let linha = $('<tr>');
+    let colunaUsuario = $('<td>').text(nomeUsuario);
+    let colunaPalavras = $('<td>').text(numerosPalavras);
+    let colunaRemover = $('<td>');
+
+    let link = $('<a>').addClass('botao-remover').attr('href', '#');
+    let icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
+
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha() {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
