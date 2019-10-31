c4a06a7436084226cee09ff0d8c191081171366a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-10-31 07:14:02 -0300

    conclusão do módulo 01 da aula 03 da seção "JavaScript: Projeto de conclusão"

---
+++ Carreiras/01-Desenvolvedor-JavaScript/08-Projeto-Conclusao/03-Logica-Jogo/js/jogo.js
@@ -0,0 +1,19 @@
+var criaJogo = function () {
+    var setPalavraSecreta = function () {
+
+    };
+
+    var getLacunas = function () {
+
+    };
+
+    var getEstapa = function () {
+
+    };
+
+    return {
+        setPalavraSecreta,
+        getLacunas,
+        getEstapa
+    };
+};
