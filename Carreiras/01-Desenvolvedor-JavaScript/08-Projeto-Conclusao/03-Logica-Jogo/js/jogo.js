66451702e2bf306ae724abfe2ba289e42123d0ab
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-02 19:03:18 -0300

    conclusão do módulo 02 da aula 03 da seção "JavaScript: Projeto de conclusão"

--- Carreiras/01-Desenvolvedor-JavaScript/08-Projeto-Conclusao/03-Logica-Jogo/js/jogo.js
+++ Carreiras/01-Desenvolvedor-JavaScript/08-Projeto-Conclusao/03-Logica-Jogo/js/jogo.js
@@ -1,15 +1,39 @@
 var criaJogo = function () {
-    var setPalavraSecreta = function () {
+    var etapa = 1;
+    var lacunas = [];
+    var palavraSecreta = '';
 
+    // adiciona uma lacuna em branco para cada letra da palavraSecreta
+    var criaLacunas = function () {
+        for (let i = 0; i < palavraSecreta.length; i++) {
+            lacunas.push('');
+        }
+    };
+
+    // muda o estado da variável etapa para indicar a próxima e última etapa
+    var proximaEtapa = function () {
+        etapa = 2;
+    };
+
+    var setPalavraSecreta = function (palavra) {
+        palavraSecreta = palavra;
+        criaLacunas();
+        proximaEtapa();
     };
 
     var getLacunas = function () {
-
+        return lacunas;
     };
 
     var getEstapa = function () {
-
+        return etapa;
     };
+
+    /* 
+    Tornou acessível apenas as funções que fazem sentido serem chamadas por quem utilizar nosso jogo. 
+        A função `proximaEtapa()` é de uso interno e só foi criada para melhorar a legibilidade e manutenção do código, a 
+        mesma coisa para a função `criaLacunas()`. 
+    */
 
     return {
         setPalavraSecreta,
